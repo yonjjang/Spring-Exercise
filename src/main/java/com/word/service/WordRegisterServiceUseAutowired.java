bd3507f5e2d630326bf7d5e8e09e0ea124f2309f
yonjjang <fic1214@gmail.com>
2021-04-17 23:48:38 +0900

    qualified attribute is added 20210417

--- src/main/java/com/word/service/WordRegisterServiceUseAutowired.java
+++ src/main/java/com/word/service/WordRegisterServiceUseAutowired.java
@@ -8,9 +8,9 @@ import com.word.dao.WordDao;
 
 public class WordRegisterServiceUseAutowired {
 
-	@Autowired
-//	@Qualifier("usedDao")
-	private WordDao wordDao;
+	@Autowired (required = false) // required = false 이면 해당 객체가 없을 때 Autowired 안 함 (Exception 피하기)
+	@Qualifier("usedDao") // appCtx 내 qualifier 속성 setting 후 해당 객체를 가르키게 함 
+	private WordDao wordDao; // 객체 이름이 같으면 우선적으로 해당 객체로 Autowired 해줌 (그러나 좋은 방법이 아님)
 	
 	// 생성자에 Autowired 없이 Property나 Method에 Autowired 사용 시, Default 생성자 꼭 선언 필요
 	public WordRegisterServiceUseAutowired() {
